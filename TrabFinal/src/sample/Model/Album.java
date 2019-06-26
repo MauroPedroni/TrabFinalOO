3723e397c3c3358cb25b90e6e4442aedaeeb6c7d
MauroPedroni <51933780+MauroPedroni@users.noreply.github.com>
2019-06-26 04:24:11 -0300

    Add files via upload

---
+++ TrabFinal/src/sample/Model/Album.java
@@ -0,0 +1,41 @@
+package sample.Model;
+
+import java.util.ArrayList;
+
+public class Album {
+    public Album(String nome, String musicas, String autor) {
+        this.nome = nome;
+        this.musicas = musicas;
+        this.autor = autor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(String musicas) {
+        this.musicas = musicas;
+    }
+
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    private String nome;
+    private String musicas;
+
+    private String autor;
+}
